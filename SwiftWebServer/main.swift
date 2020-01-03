2ab482c36c40d289d196e9151bd410a1244a0c6a
Quirin Schweigert <quirin.schweigert@tum.de>
2020-01-03 22:04:18 +0100

    Initial Commit

---
+++ SwiftWebServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftWebServer
+//
+//  Created by Quirin Schweigert on 03.01.20.
+//  Copyright © 2020 Quirin Schweigert. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
